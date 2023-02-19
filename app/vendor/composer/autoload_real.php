13a764a96eaca13b8b68a8575f35266becf41b97
chidodesigns <chidoukaigwe@gmail.com>
2023-02-19 19:48:06 +0000

    added env variables

--- app/vendor/composer/autoload_real.php
+++ app/vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInit40de15851339b8a79278b2009a253c13
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInit40de15851339b8a79278b2009a253c13', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInit40de15851339b8a79278b2009a253c13', 'loadClassLoader'));
@@ -30,6 +32,18 @@ class ComposerAutoloaderInit40de15851339b8a79278b2009a253c13
         call_user_func(\Composer\Autoload\ComposerStaticInit40de15851339b8a79278b2009a253c13::getInitializer($loader));
 
         $loader->register(true);
+
+        $filesToLoad = \Composer\Autoload\ComposerStaticInit40de15851339b8a79278b2009a253c13::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
+        }
 
         return $loader;
     }
